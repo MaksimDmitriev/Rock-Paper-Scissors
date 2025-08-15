2f9c4ea9920c3310f27a9bdf02114d7bdea59637
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2025-08-15 19:55:51 +0800

    Add watch_computer_playing and play_with_computer

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import random
+import time
 
+MODES = ["C", "U"]
 MOVES = ["R", "P", "S"]
 
-if __name__ == "__main__":
-    print("This is Rock, Paper, Scissors game. Input R, P, or S, capital or small, and see who wins!\n")
 
+def play_with_computer():
     try:
         while True:
             user_input = input("Enter R, P, or S (or Q to quit): ").strip().upper()
@@ -18,7 +19,7 @@ if __name__ == "__main__":
 
             computer_choice = random.choice(MOVES)
 
-            match(user_input, computer_choice):
+            match (user_input, computer_choice):
                 case ("R", "S") | ("P", "R") | ("S", "P"):
                     print(f"You chose {user_input}, computer chose {computer_choice}. You win!")
                 case ("S", "R") | ("R", "P") | ("P", "S"):
@@ -28,3 +29,36 @@ if __name__ == "__main__":
 
     except KeyboardInterrupt:
         print("\nGoodbye!")
+
+
+def watch_computer_playing():
+    try:
+        while True:
+            computer_choice1 = random.choice(MOVES)
+            computer_choice2 = random.choice(MOVES)
+            time.sleep(1)
+            match (computer_choice1, computer_choice2):
+                case ("R", "S") | ("P", "R") | ("S", "P"):
+                    print(f"Computer 1 chose {computer_choice1}, Computer 2 chose {computer_choice2}. Computer 1 wins!")
+                case ("S", "R") | ("R", "P") | ("P", "S"):
+                    print(f"Computer 1 chose {computer_choice1}, Computer 2 chose {computer_choice2}. Computer 2 wins!")
+                case _:
+                    print(f"Both computers chose {computer_choice1}. It's a tie!")
+    except KeyboardInterrupt:
+        print("\nGoodbye!")
+
+
+if __name__ == "__main__":
+    game_mode = input(
+        "Enter C to watch the computer playing with itself or U to play with the computer: ").strip().upper()
+    while True:
+        if game_mode in MODES:
+            break
+        print("Invalid input. Input C or U, capital or small")
+
+    if game_mode == "C":
+        print("Computer will play with itself")
+        watch_computer_playing()
+    elif game_mode == "U":
+        print("This is Rock, Paper, Scissors game. Input R, P, or S, capital or small, and see who wins!\n")
+        play_with_computer()
